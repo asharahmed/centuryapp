aee90413b0404cafb9fa94ac87561187dbed3c59
Ashar Ahmed <9995806+asharahmed@users.noreply.github.com>
2023-04-07 14:38:45 -0400

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,63 @@
 import React from 'react';
-import { Card, CardContent, Typography,  } from '@mui/material';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { blue } from '@mui/material/colors';
+import { Card, CardContent, Typography } from '@mui/material';
 import Visualization from './visualization';
+import { makeStyles } from '@mui/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Header from './Header';
+import {CssBaseline } from '@mui/material';
+
 
 const theme = createTheme({
   palette: {
-    primary: {
-      main: blue[500],
-    },
     background: {
-      default: '#fafafa',
+      default: '#fff', // Set a default background color
     },
-  },
-  typography: {
-    fontFamily: ['Roboto', 'sans-serif'].join(','),
   },
 });
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundColor: theme.palette.background.default,
+  },
+  card: {
+    minWidth: 275,
+    padding: theme.spacing(3),
+    borderRadius: theme.shape.borderRadius,
+    boxShadow: theme.shadows[3],
+  },
+  title: {
+    marginBottom: theme.spacing(2),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
-    <ThemeProvider theme={theme}>
-      <Header />
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <Card sx={{ minWidth: 275, padding: 3, borderRadius: 2, boxShadow: 3 }}>
-          <CardContent>
-            <Typography fontFamily={['Roboto', 'sans-serif']} variant="h5" component="h2" sx={{ marginBottom: 2 }}>
-              Century Initiative Visualization
-            </Typography>
-            <Visualization />
-          </CardContent>
-        </Card>
-      </div>
-    </ThemeProvider>
+    <div className={classes.root}>
+      <Card className={classes.card}>
+        <CardContent>
+          <Typography variant="h5" component="h2" className={classes.title}>
+            Visualization
+          </Typography>
+          <Visualization />
+        </CardContent>
+      </Card>
+    </div>
   );
 }
 
-export default App;
-
+export default function ThemedApp() {
+  return (
+    
+    <ThemeProvider theme={theme}>
+      <Header/>
+      <App />
+    </ThemeProvider>
+  
+  );
+}
