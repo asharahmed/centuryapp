49f63480cb3d46bc9fd17394ef3928509ceae1fb
Ashar Ahmed <9995806+asharahmed@users.noreply.github.com>
2023-04-10 05:14:32 -0400

    push

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: 'public',
-    register: true,
-    scope: '/',
-    sw: 'service-worker.js',
-    disable: process.env.NODE_ENV === 'development'
-  }
-})
+module.exports = nextConfig
