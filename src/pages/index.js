03eb877a6c226c9e0cf6a8635e957214706271d2
Ashar Ahmed <9995806+asharahmed@users.noreply.github.com>
2023-04-10 14:15:27 -0400

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -230,9 +230,10 @@ function App() {
             </Typography>
           <Typography variant="body1" component="p">
             <b>As we grow, we need to make sure that our public services are able to scale up with us. </b>
-            <br/><br/>This can be done responsibly - The Century Initiative aims to engage Canadians in a national conversation about the benefits
-            and challenges of population growth, and to develop a long-term vision and strategy for achieving this ambitious goal, by making sure that
-            our public services are able to scale up with us by ensuring a well-educated and well-trained workforce and raising wages to attract and retain talent.
+            <br/><br/>This can be done responsibly - progressives aim to engage Canadians in a national conversation about the benefits
+            and challenges of population growth, and to develop a long-term vision and strategy for achieving this ambitious goal. By making sure that
+            our public services are able to scale up with us by ensuring a well-educated and well-trained workforce and raising wages to attract and retain talent, 
+            we can build a stronger, more prosperous Canada.
           <br/>
           
             </Typography>
