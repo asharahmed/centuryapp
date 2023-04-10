54551a32bb83af80d0d0d998a8cd2b4316f4bb48
Ashar Ahmed <9995806+asharahmed@users.noreply.github.com>
2023-04-10 05:08:33 -0400

    push

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = withPWA({
+  ...nextConfig,
+  pwa: {
+    dest: 'public',
+    register: true,
+    scope: '/',
+    sw: 'service-worker.js',
+    disable: process.env.NODE_ENV === 'development'
+  }
+})
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 
+
 export default function App({ Component, pageProps }) {
+<link rel="manifest" href="../../public/manifest.json" />
   return <Component {...pageProps} />
 }
