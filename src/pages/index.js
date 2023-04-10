57f3746b69bff1adaee7cbcb1a46b1665445871c
Ashar Ahmed <9995806+asharahmed@users.noreply.github.com>
2023-04-10 05:49:18 -0400

    push

--- src/pages/index.js
+++ src/pages/index.js
@@ -171,7 +171,7 @@ function App() {
             The Century Initiative is a group of Canadians committed to building a prosperous, vibrant, and globally influential Canada by growing the population to 100 million by 2100.
             <br/><br/>
             To find out more, visit their website at <a href="https://www.centuryinitiative.ca/">https://www.centuryinitiative.ca/.</a>
-            <br/><br/><b>ⓘ This web app is not associated with the Century Initative.</b> <a href="https://github.com/asharahmed/centuryapp">Learn more.</a>
+            <br/><br/><b>ⓘ This web app is not associated with the Century Initative.</b> <a href="https://github.com/asharahmed/centuryapp"><u>Learn more.</u></a>
           </Typography>
         </CardContent>
       </Card>
