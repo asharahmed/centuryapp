5fea4e5572871416c03c2389156ea4bb6f8cb912
Ashar Ahmed <9995806+asharahmed@users.noreply.github.com>
2023-04-10 15:59:32 -0400

    initial commit

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -39,6 +39,7 @@ const useStyles = makeStyles((theme = { palette: {}}) => ({
                 </Typography>
             </Toolbar>
             </AppBar>
+            <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>📈</text></svg>"/>
         </header>
         )
     );
